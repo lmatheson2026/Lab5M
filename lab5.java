d8ae5e253531c15f0f6b903289a50c1326a962e9
lmatheson2026 <144724348+lmatheson2026@users.noreply.github.com>
2023-10-23 14:53:24 -0400

    Add files via upload

---
+++ lab5.java
@@ -0,0 +1,13 @@
+
+public class lab5 {
+
+	public static void main(String[] args) {
+	myBinarySearchTree Y = new myBinarySearchTree();
+	myTreeIterator X = new myTreeIterator();
+	TreeIteratorTesting = new TreeIteratorTesting();
+
+
+
+	}
+
+}
